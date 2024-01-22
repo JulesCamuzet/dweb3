39e8bb77d5032a82391a303e1ccbe0c088816547
NoahDED <noah.dedina@outlook.fr>
2024-01-22 11:01:50 +0100

    Noah-commit38emetest

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const jules = document.getElementById("jules")
 jules.addEventListener('mouseover', () => {
   console.log("Jules !")
 })
+
+const paragraphs = document.querySelectorAll("p");
+
+paragraphs.forEach((para) => {
+  para.addEventListener("mouseover", (event) => {
+    console.log(`le plsu beau => "${event.target.id}" `);
+  });
+});
